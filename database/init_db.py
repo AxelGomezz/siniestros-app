1f34fe0bf01ff37d10f535ed4f8a3881061726a1
Axel-ubuntuWsl <axelrg09@gmail.com>
2025-06-27 20:25:06 +0000

    creacion de la tabla siniestros

--- database/init_db.py
+++ database/init_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Crea / conecta la base de datos
-conn = sqlite3.connect("siniestros.db")
+conn = sqlite3.connect("database/siniestros.db")
 
 # crea el cursor con el cual se ejecutan comandos SQL
 cursor = conn.cursor()
@@ -11,6 +11,19 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS clients (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
+)
+"""
+)
+
+#Crea tabla siniestros
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS siniestros (
+    id INTEGER PRIMARY KEY,
+    client_id INTEGER NOT NULL,
+    date TEXT NOT NULL,
+    patente TEXT,
+    descripcion TEXT,
+    FOREIGN KEY (client_id) REFERENCES clients(id)
 )
 """
 )
